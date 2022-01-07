4a001a55720c822446fc012ab9637586dce76f14
urvashikharecha <urvashi.k@7span.com>
2022-01-07 15:46:57 +0530

    UI screen for listing news

---
+++ lib/ui/news/news_list.dart
@@ -0,0 +1,146 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_boilerplate/constants/app_colors.dart';
+import 'package:flutter_boilerplate/model/news_api_models/Article.dart';
+import 'package:flutter_boilerplate/ui/news/bloc/news_bloc.dart';
+import 'package:flutter_boilerplate/ui/news/bloc/news_event.dart';
+import 'package:flutter_boilerplate/ui/news/bloc/news_state.dart';
+import 'package:flutter_boilerplate/utils/app_utils.dart';
+import 'package:sizer/sizer.dart';
+
+class NewsListScreen extends StatefulWidget {
+  const NewsListScreen({Key? key}) : super(key: key);
+
+  static Widget create() {
+    return BlocProvider<NewsBloc>(
+      create: (_) => NewsBloc()..add(NewsListEvent()),
+      child: const NewsListScreen(),
+    );
+  }
+
+  @override
+  State<NewsListScreen> createState() => _NewsListScreenState();
+}
+
+class _NewsListScreenState extends State<NewsListScreen> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: BlocListener<NewsBloc, NewsState>(
+        listener: (context, state) {
+          if (state.isCompleted) {
+            if (state.isEmailVerified) {
+              //  _navigateToHome(context);
+            } else {
+              //_navigateToVerifyEmail(context);
+            }
+          }
+          AppUtils.showSnackBar(context, state.error?.getFirstError());
+        },
+        child: BlocBuilder<NewsBloc, NewsState>(
+          builder: (context, state) {
+            List<Article> articleList = state.newsResponse?.articles ?? [];
+            return state.isLoading
+                ? const Center(
+                    child: CircularProgressIndicator(
+                      color: AppColors.white,
+                    ),
+                  )
+                : articleList.isNotEmpty
+                    ? Container(
+                        padding: const EdgeInsets.only(bottom: 30),
+                        child: ListView.builder(
+                          shrinkWrap: true,
+                          scrollDirection: Axis.vertical,
+                          padding: EdgeInsets.symmetric(vertical: 32.sp),
+                          itemCount: articleList.length,
+                          itemBuilder: (context, index) {
+                            return InkWell(
+                              child: Container(
+                                margin: const EdgeInsets.all(10),
+                                child: Card(
+                                  child: Row(
+                                    children: [
+                                      Padding(
+                                        padding: const EdgeInsets.all(5),
+                                        child: Image.network(
+                                          articleList[index].urlToImage ?? ' ',
+                                          height: 100,
+                                          width: 100,
+                                        ),
+                                      ),
+                                      Flexible(
+                                        child: Column(
+                                          crossAxisAlignment:
+                                              CrossAxisAlignment.start,
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.start,
+                                          children: [
+                                            Padding(
+                                              padding: const EdgeInsets.all(5),
+                                              child: Text(
+                                                articleList[index].title ?? '',
+                                                style: const TextStyle(
+                                                    fontSize: 16,
+                                                    fontWeight: FontWeight.bold,
+                                                    color: Colors.green),
+                                              ),
+                                            ),
+                                            Padding(
+                                              padding: const EdgeInsets.all(5),
+                                              child: Text(
+                                                articleList[index]
+                                                        .description ??
+                                                    '',
+                                                style: const TextStyle(
+                                                    fontSize: 12,
+                                                    fontWeight:
+                                                        FontWeight.normal,
+                                                    color: AppColors.color5),
+                                              ),
+                                            )
+                                          ],
+                                        ),
+                                      )
+                                    ],
+                                  ),
+                                ),
+                              ),
+                              onTap: () {},
+                            );
+                          },
+                        ),
+                      )
+                    : Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Container(
+                            height: 200,
+                            child: Center(
+                              child: Text("No Articles found!!",
+                                  style: TextStyle(
+                                    color: AppColors.white,
+                                    fontWeight: FontWeight.normal,
+                                    fontSize: 24.sp,
+                                  )),
+                            ),
+                          )
+                        ],
+                      );
+          },
+        ),
+      ),
+    );
+  }
+}
