8b58e71de10f42a406d800014772ff1d310fc8f8
urvashikharecha <urvashi.k@7span.com>
2022-01-28 11:22:44 +0530

    Add configuration to production

--- lib/main_prod.dart
+++ lib/main_prod.dart
@@ -1,10 +1,17 @@
 import 'dart:async';
-import 'package:flutter/material.dart';
 
+import 'package:flutter/material.dart';
 import 'package:flutter_boilerplate/app_config.dart';
 import 'package:flutter_boilerplate/main.dart';
+import 'package:flutter_boilerplate/utils/app_dependencies.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
 
 void main() async {
+  await initHiveForFlutter();
+
+  WidgetsFlutterBinding.ensureInitialized();
+
+  setupDependencies();
   var configuredApp = AppConfig(
       appName: 'Flutter BoilerPlate',
       flavorName: 'prod',
